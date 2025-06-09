fc9526f587ba2aa8bc76b5d331a1f2d2824ffdea
Aman <aman13032006@gmail.com>
2025-06-09 06:06:56 +0600

    commit

--- weather/views.py
+++ weather/views.py
@@ -12,21 +12,3 @@ class WeatherRecordView(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['city__name', 'city__region__name', 'city__region__country__name']
     search_fields = ['description', 'city__name']
-
-    def perform_create(self, serializer):
-        """
-        Ожидает `city_id` в POST-запросе. Остальные данные генерируются.
-        """
-        city_id = self.request.data.get('city_id')
-        city = get_object_or_404(City, id=city_id)
-
-        temperature = round(random.uniform(15.0, 30.0), 1)
-        description = random.choice(["Солнечно", "Облачно", "Дождливо", "Снег"])
-        humidity = random.randint(40, 80)
-
-        serializer.save(
-            city=city,
-            temperature=temperature,
-            description=description,
-            humidity=humidity
-        )
